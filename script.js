bbe140870f3f4eb2de65c840c2007f8656e0a57a
qsoft4dblind <dwitra.qsoft4dblind@gmail.com>
2024-12-09 21:23:20 +0700

    branch

---
+++ script.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMHJEtWpbKejtN9weGuLd0KNtWYZAfMy4",
+    authDomain: "dpd-pertuni.firebaseapp.com",
+    databaseURL: "https://dpd-pertuni-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "dpd-pertuni",
+    storageBucket: "dpd-pertuni.appspot.com",
+    messagingSenderId: "253614860627",
+    appId: "1:253614860627:web:8fa7856e56c9cfd84359a6",
+    measurementId: "G-6J7XZB9R8K"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
